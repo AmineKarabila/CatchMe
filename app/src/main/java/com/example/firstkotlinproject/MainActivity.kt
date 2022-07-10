4ad55be660bdc214fc90e1ba9ea60d70ab8e489c
Amine Karabila <aminfifa10@hotmail.com>
2022-07-10 16:41:07 +0200

    MainActivity überarbeitet

--- app/src/main/java/com/example/firstkotlinproject/MainActivity.kt
+++ app/src/main/java/com/example/firstkotlinproject/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.firstkotlinproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_main) // Damit geben wir an das die main_activity angegeben werden soll
+        supportActionBar?.hide()
+    }
+
+    fun openSettings(view: View){
+        val cangePage = Intent(this, Settings::class.java)
+        startActivity(cangePage)
+    }
+    fun openHelp(view: View){
+        val cangePage = Intent(this, Help::class.java)
+        startActivity(cangePage)
     }
 }
---
+++ app/src/main/java/com/example/firstkotlinproject/Settings.kt
@@ -0,0 +1,21 @@
+package com.example.firstkotlinproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+
+class Settings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_main) // Damit geben wir an das die main_activity angegeben werden soll
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_settings)
+    }
+    fun openMain(view: View){
+        val cangePage = Intent(this, MainActivity::class.java)
+        startActivity(cangePage)
+    }
+}
